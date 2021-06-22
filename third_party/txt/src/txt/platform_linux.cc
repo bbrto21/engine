cf361f21a29b14f69b99a3d9801aac60e8c56da9
Boram Bae <boram21.bae@samsung.com>
2021-06-22 13:10:19 +0900

    Revert "Fix font breaking issues (#69)"
    This reverts commit 5552dd5b29bef253dad86892c2e4b2540d7466d6.

--- third_party/txt/src/txt/platform_linux.cc
+++ third_party/txt/src/txt/platform_linux.cc
@@ -13,76 +13,14 @@
 namespace txt {
 
 std::vector<std::string> GetDefaultFontFamilies() {
-#ifdef FLUTTER_USE_FONTCONFIG
   return {"TizenDefaultFont"};
-#else
-  return {
-      "SamsungOneUI",
-      "SamsungOneUIArabic",
-      "SamsungOneUIArmenian",
-      "SamsungOneUIBangla",
-      "SamsungOneUIDevanagari",
-      "SamsungOneUIEthiopic",
-      "SamsungOneUIFallback",
-      "SamsungOneUIGeorgian",
-      "SamsungOneUIGujarati",
-      "SamsungOneUIGurmukhi",
-      "SamsungOneUIHebrew",
-      "SamsungOneUIJP",
-      "SamsungOneUIKannada",
-      "SamsungOneUIKhmer",
-      "SamsungOneUIKorean",
-      "SamsungOneUIKoreanH",
-      "SamsungOneUILao",
-      "SamsungOneUIMalayalam",
-      "SamsungOneUIMyanmar",
-      "SamsungOneUIOdia",
-      "SamsungOneUIOlChiki",
-      "SamsungOneUISCN",
-      "SamsungOneUISinhala",
-      "SamsungOneUITCN",
-      "SamsungOneUITagalog",
-      "SamsungOneUITamil",
-      "SamsungOneUITelugu",
-      "SamsungOneUIThai",
-      "SamsungOneFallback",
-      "SECEmoji",
-      "BreezeSans",
-      "BreezeSansArabic",
-      "BreezeSansArmenian",
-      "BreezeSansBengali",
-      "BreezeSansChinese",
-      "BreezeSansEthiopic",
-      "BreezeSansGeorgian",
-      "BreezeSansGujarathi",
-      "BreezeSansHebrew",
-      "BreezeSansHindi",
-      "BreezeSansJapanese",
-      "BreezeSansKannada",
-      "BreezeSansKhmer",
-      "BreezeSansKorean",
-      "BreezeSansLao",
-      "BreezeSansMalayalam",
-      "BreezeSansMeeteiMayek",
-      "BreezeSansMyanmar",
-      "BreezeSansOriya",
-      "BreezeSansPunjabi",
-      "BreezeSansSinhala",
-      "BreezeSansTamilBreezeSansTamil",
-      "BreezeSansTelugu",
-      "BreezeSansThai",
-      "BreezeSansTibetan",
-      "BreezeSansFallback",
-      "BreezeColorEmoji",
-  };
-#endif
 }
 
 sk_sp<SkFontMgr> GetDefaultFontManager() {
 #ifdef FLUTTER_USE_FONTCONFIG
   return SkFontMgr::RefDefault();
 #else
-  return SkFontMgr_New_Custom_Directory("/usr/share/");
+  return SkFontMgr_New_Custom_Directory("/usr/share/fonts/");
 #endif
 }
 
